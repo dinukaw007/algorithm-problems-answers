90747ef5b599a5ee098464363fd206434a27b7f0
Dinuka Wanasinghe <dinukaw@masholdings.com>
2018-11-07 17:46:50 +0530

    Add ZigZagConversion Program

---
+++ AlgorithmAnswers/ZigZagConversion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ZigZagConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var convert_string = Convert("PAYPALISHIRING", 3);
+            Console.WriteLine(convert_string);
+            Console.ReadLine();
+        }
+
+        private static string Convert(string s,  int numRows)
+        {
+            if (numRows == 1) return s;
+            string ret = "";
+            int n = s.Length;
+            char[] sArray = s.ToCharArray();
+            int cycelLen = 2 * numRows - 2;
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j + i < n; j += cycelLen)
+                {
+                    ret = string.Concat(ret, sArray[j + i]);
+                    if (i != 0 && i != numRows - 1 && j + cycelLen - i < n)
+                    {
+                        ret += string.Concat(sArray[j + cycelLen - i]);
+                    }
+                }
+            }
+            return ret;
+        }
+    }
+}
---
+++ AlgorithmAnswers/ZigZagConversion/README.md
@@ -0,0 +1,19 @@
+﻿## ZigZag Conversion
+String
+
+The string "PAYPALSISHIRING" is written in a zigzag pattern on a given numbers of rows like this : (you may want to dispaly this pattern in a fixe font for better legibility)
+```
+    P   A   H   N
+    A P L S I I G
+    Y   I   R
+```    
+    
+And then read line by line :
+"PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows :
+string covert (string tex , int nRows);
+
+```
+convert("PAHNAPLSIIGYIR",3) should return "PAHNAPLSIIGYIR"
+```
