c485ab592dff1376896c18b1392c7a86600a80e1
dinukaw007 <wanasinghe007@gmail.com>
2018-11-10 23:58:02 +0530

    Palindrome Number program with tow methods and unit test.

---
+++ AlgorithmAnswers/AlgorithmAnswersTest/PalindromeNumberUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PalindromeNumber;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmAnswersTest
+{
+    [TestClass]
+    public class PalindromeNumberUnitTest
+    {
+        [TestMethod]
+        public void PalindromeNumberTestMethod1()
+        {
+            bool value = Palindrome.IsPalindrome(123);
+            Assert.AreEqual(false, value);
+        }
+
+        [TestMethod]
+        public void PalindromeNumberTestMethod2()
+        {
+            bool value = Palindrome.IsPalindrome(0);
+            Assert.AreEqual(true, value);
+        }
+
+        [TestMethod]
+        public void PalindromeNumberTestMethod3()
+        {
+            bool value = Palindrome.IsPalindrome(-121);
+            Assert.AreEqual(false, value);
+        }
+
+        [TestMethod]
+        public void PalindromeNumberTestMethod5()
+        {
+            bool value = Palindrome.IsPalindrome(121);
+            Assert.AreEqual(true, value);
+        }
+    }
+}
---
+++ AlgorithmAnswers/PalindromeNumber/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = 121;
+            Console.WriteLine("Value : " + val);
+            var IsPalindrome  = Palindrome.IsPalindrome(val);
+            Console.WriteLine("Is Palindrome : " + IsPalindrome);
+            Console.ReadLine();
+
+        }
+
+       
+    }
+
+    public static class Palindrome
+    {
+        //96 ms run time
+        public static bool IsPalindrome(int x)
+        {
+            if (x < int.MaxValue && x > 0 && (x % 10 == 0 && x != 0))
+                return false;
+            int revertNumber = 0;
+            while (x > revertNumber)
+            {
+                revertNumber = revertNumber * 10 +x % 10;
+                x /= 10;
+            }
+            return x == revertNumber || x == revertNumber / 10;
+        }
+
+        //100 ms run time
+        public static bool IsPalindromeTwo(int x)
+        {
+            if (x < int.MaxValue && x >= 0 && x % 10 == 0)
+                return false;
+            int revertNumber = 0;
+            int tepm = x;
+            while (tepm > 0)
+            {
+                revertNumber = revertNumber * 10 + tepm % 10;
+                tepm /= 10;
+            }
+            return revertNumber == x;
+        }
+    }
+}
